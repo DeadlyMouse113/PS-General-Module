783caaac44e402334bcc9f28adc731347eacb30b
Jochen Kerremans <jochenkerremans@hotmail.com>
2023-03-25 11:40:33 +0100

    Adding module and functions

---
+++ PSGeneralImportModule.psm1
@@ -0,0 +1,20 @@
+﻿# Universal psm file
+# Requires -Version 5.0
+
+# Get functions files
+$Functions = @( Get-ChildItem -Path $PSScriptRoot\scripts\*.ps1 -ErrorAction SilentlyContinue)
+
+# Dot source the files
+Foreach($import in @($Functions))
+{
+    Try{
+        . $import.fullname
+    }
+    Catch{
+        Write-Error -Message "Failed to import function $($import.fullname): $_"
+    }
+
+}
+
+# Export everything in the public folder
+Export-ModuleMember -Function * -Cmdlet * -Alias *
---
+++ scripts/Get-Dates.ps1
@@ -0,0 +1,9 @@
+﻿# Get-NextWeekDate
+
+function Get-NextWeekDate{
+
+    $nextweek = (Get-Date).AddDays(7)
+    Write-Output $nextweek
+}
+
+Export-ModuleMember -Function Get-NextWeekDate
